5d8ccff6adeb00f1a52cb3bd7bec3ab212a53e17
Blowa <blowaxd693@gmail.com>
2023-07-16 12:51:35 +0200

    fix: some fixes on events spamming (#61)
    * fix: some fixes on events spamming
    
    * bump version
    
    ---------
    
    Co-authored-by: Pumba98 <mail@pumba98.de>

--- src/Bitwarden.SecretOperator/CRDs/Secret/BitwardenSecretController.cs
+++ src/Bitwarden.SecretOperator/CRDs/Secret/BitwardenSecretController.cs
@@ -20,7 +20,8 @@ public class BitwardenSecretController : ControllerBase, IResourceController<Bit
     private readonly BitwardenCliWrapper _cliWrapper;
     private readonly IEventManager _eventManager;
 
-    public BitwardenSecretController(ILogger<BitwardenSecretController> logger, KubernetesClient kubernetesClient, BitwardenCliWrapper cliWrapper, IOptions<BitwardenOperatorOptions> operatorOptions, IEventManager eventManager)
+    public BitwardenSecretController(ILogger<BitwardenSecretController> logger, KubernetesClient kubernetesClient, BitwardenCliWrapper cliWrapper, IOptions<BitwardenOperatorOptions> operatorOptions,
+        IEventManager eventManager)
     {
         _logger = logger;
         _kubernetesClient = kubernetesClient;
@@ -31,38 +32,38 @@ public class BitwardenSecretController : ControllerBase, IResourceController<Bit
 
     public async Task<ResourceControllerResult?> ReconcileAsync(BitwardenSecretCrd entity)
     {
-            BitwardenSecretSpec spec = entity.Spec;
+        BitwardenSecretSpec spec = entity.Spec;
 
-            string? destinationName = spec.Name ?? entity.Name();
-            string? destinationNamespace = spec.Namespace ?? entity.Namespace();
+        string? destinationName = spec.Name ?? entity.Name();
+        string? destinationNamespace = spec.Namespace ?? entity.Namespace();
         try
         {
             await _eventManager.PublishAsync(entity, "bitwarden-secret-operator", "Received reconcile");
             var secret = await _kubernetesClient.Get<V1Secret>(destinationName, destinationNamespace);
             if (secret == null)
             {
-                await _eventManager.PublishAsync(entity, "bitwarden-secret-operator", $"Couldn't find secret {destinationName} in namespace {destinationNamespace}, creating it...");
                 _logger.LogInformation("Secret: {SecretName} in namespace: {Namespace} couldn't be found, creating it", destinationName, destinationNamespace);
+
                 // create
                 secret = await entity.GetSecretAsync(_cliWrapper);
                 secret = await _kubernetesClient.Create<V1Secret>(secret);
-                
+
                 // created events
-                await _eventManager.PublishAsync(secret, "bitwarden-secret-operator", $"Secret {destinationName} in namespace {destinationNamespace}, created");
-                await _eventManager.PublishAsync(entity, "bitwarden-secret-operator", $"Secret {destinationName} in namespace {destinationNamespace}, creation successful");
+                await _eventManager.PublishAsync(secret, "Created", $"Secret {destinationName} in namespace {destinationNamespace}, created");
                 _logger.LogInformation("Secret: {SecretName} in namespace: {Namespace} created", destinationName, destinationNamespace);
             }
             else
             {
-                await _eventManager.PublishAsync(entity, "bitwarden-secret-operator", $"Secret {destinationName} in namespace {destinationNamespace}, updating it...");
+                await _eventManager.PublishAsync(secret, "Updating", $"Secret {destinationName} in namespace {destinationNamespace}, updating it...");
                 _logger.LogInformation("Secret: {SecretName} in namespace: {Namespace} exists, updating it", destinationName, destinationNamespace);
+
+                // update
                 secret = await entity.GetSecretAsync(_cliWrapper);
                 await _kubernetesClient.Update<V1Secret>(secret);
-                
+
                 // updated events
-                await _eventManager.PublishAsync(entity, "bitwarden-secret-operator", $"Secret {destinationName} in namespace {destinationNamespace}, updated!");
-                await _eventManager.PublishAsync(secret, "bitwarden-secret-operator", $"Secret {destinationName} in namespace {destinationNamespace}, updated");
-                
+                await _eventManager.PublishAsync(secret, "Updated", $"Secret {destinationName} in namespace {destinationNamespace}, updated!");
+
                 _logger.LogInformation("Secret: {SecretName} in namespace: {Namespace} updated", destinationName, destinationNamespace);
             }
 
@@ -71,21 +72,21 @@ public class BitwardenSecretController : ControllerBase, IResourceController<Bit
         }
         catch (Exception e)
         {
-            await _eventManager.PublishAsync(entity, "bitwarden-secret-operator", $"Secret {destinationName} in namespace {destinationNamespace}, failed to create, check operator logs");
+            await _eventManager.PublishAsync(entity, "Failed", $"Secret {destinationName} in namespace {destinationNamespace}, failed to create, check operator logs", EventType.Warning);
             _logger.LogError(e, "[{Method}] Failed", nameof(ReconcileAsync));
             // requeue the event in 15 seconds
             if (_operatorOptions.DelayAfterFailedWebhook is null)
             {
                 throw;
             }
-                
+
             return ResourceControllerResult.RequeueEvent(_operatorOptions.DelayAfterFailedWebhook.Value);
         }
     }
 
-    public async Task StatusModifiedAsync(BitwardenSecretCrd entity)
+    public Task StatusModifiedAsync(BitwardenSecretCrd entity)
     {
-        return;
+        return Task.CompletedTask;
     }
 
     public async Task DeletedAsync(BitwardenSecretCrd entity)
